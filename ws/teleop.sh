9cf9be5dd563152ce83bd5f8b1f9723ce1c5b136
malenwater <128200788+malenwater@users.noreply.github.com>
2025-02-18 22:48:11 +0900

    2개 이미지 확인 가능
    한 것
    1. 하나의 터틀봇의 이미지 확인 가능
    할 것
    1. 이를 확인할 tel 과 bash 파일 정리하기

--- ws/teleop.sh
+++ ws/teleop.sh
@@ -1,5 +1,2 @@
 export TURTLEBOT3_MODEL=waffle
-ros2 run turtlebot3_teleop teleop_keyboard
-
-# export TURTLEBOT3_MODEL=waffle
-# ros2 run turtlebot3_teleop teleop_keyboard --ros-args -r __ns:=/tb1
+ros2 run turtlebot3_teleop teleop_keyboard --ros-args -r __ns:=/tb1
