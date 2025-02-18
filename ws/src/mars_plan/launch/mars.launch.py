3a4fe4d2c9bc55c270cde8f089f84c469e2d793b
malenwater <128200788+malenwater@users.noreply.github.com>
2025-02-18 13:56:18 +0900

    sdf 확인

--- ws/src/mars_plan/launch/mars.launch.py
+++ ws/src/mars_plan/launch/mars.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
-        'mars.world'
+        'custom_world.world'
     )
 
 
