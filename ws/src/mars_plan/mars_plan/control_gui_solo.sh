1a37ce6f8cc82d0da9edab24da2f0fc3ad6d22f5
malenwater <128200788+malenwater@users.noreply.github.com>
2025-02-19 16:23:29 +0900

    정리 완료

--- ws/src/mars_plan/mars_plan/control_gui_solo.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-echo "🚀 GUI 실행 중..."
-source ~/Documents/GitHub/2024-2_ROKEYBOOTCAMP_Collaborative_robot_project_3/ws/install/setup.bash
-python3 ~/Documents/GitHub/2024-2_ROKEYBOOTCAMP_Collaborative_robot_project_3/ws/src/mars_plan/mars_plan/control_gui_solo.py
